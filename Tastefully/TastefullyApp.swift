9da18d3d353c8e86a76654502104c76fa69edb3e
Darie-Nistor Nicolae <41840484+darienistornicolae@users.noreply.github.com>
2022-12-13 16:52:42 +0200

    Initial Commit

---
+++ Tastefully/TastefullyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TastefullyApp.swift
+//  Tastefully
+//
+//  Created by Darie-Nistor Nicolae on 13.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct TastefullyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
